18f21dbd9c03d847ebf61bac3040b4a6153311e4
Matt Powell <fauxparse@gmail.com>
2026-08-17 21:05:58 +1200

    fix: key layout inspector icon paths (#288)

--- packages/design-system/src/icons/LayoutNoneIcon.tsx
+++ packages/design-system/src/icons/LayoutNoneIcon.tsx
@@ -6,6 +6,7 @@ export const LayoutNoneIcon = createLucideIcon("layout-none", [
     {
       d: "M4 4h6v6h-6zM14 5h6v6h-6zM8.5 15h6v6h-6z",
       stroke: "currentColor",
+      key: "layout-none",
     },
   ],
 ]);
--- packages/design-system/src/icons/LayoutVerticalIcon.tsx
+++ packages/design-system/src/icons/LayoutVerticalIcon.tsx
@@ -6,6 +6,7 @@ export const LayoutVerticalIcon = createLucideIcon("layout-vertical", [
     {
       d: "M21 17L17 21L13 17M17 21L17 3M10 4L10 10L4 10L4 4L10 4ZM10 14L10 20L4 20L4 14L10 14Z",
       stroke: "currentColor",
+      key: "layout-vertical",
     },
   ],
 ]);
