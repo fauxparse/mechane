3c2156256ba71c8686ed12996e26a8c64dfbbb95
Matt Powell <matt.powell@usabilityhub.com>
2026-09-03 14:24:26 +1200

    fix migrations broken by bad merge

--- apps/api/drizzle/0034_white_peter_parker.sql
+++ apps/api/drizzle/0034_white_peter_parker.sql
@@ -1,2 +1,3 @@
-ALTER TABLE "graph_event_bindings" ADD COLUMN "params" jsonb DEFAULT '{}'::jsonb NOT NULL;--> statement-breakpoint
-ALTER TABLE "player_events" ADD COLUMN "params" jsonb;
+ALTER TABLE "graph_event_bindings" ADD COLUMN IF NOT EXISTS "params" jsonb DEFAULT '{}'::jsonb NOT NULL;--> statement-breakpoint
+ALTER TABLE "player_events" ADD COLUMN IF NOT EXISTS "params" jsonb;--> statement-breakpoint
+ALTER TABLE "graph_nodes" ADD COLUMN IF NOT EXISTS "size" jsonb;
